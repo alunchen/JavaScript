a4b5d5b9f78aa064b7cdc8e88a2b21d1c9ca03c2
v_alunchen <v_alunchen@tencent.com>
2023-05-16 17:56:57 +0800

    fix: Promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,6 +1,7 @@
 /**
- * Promise 构造器的实现
- * 
+ * Promise A+ 规范
+ *     1. promise 是一个有then方法的对象或者是函数
+ *     2. thenable 是一个有then方法的对象或者是函数。
  */
 
 const PENDING = 'pending';
@@ -10,7 +11,7 @@ const REJECTED = 'rejected';
 class MyPromise {
   #state = 'pending';
   #result = undefined;
-  
+  #handlers = [];
   constructor(executor) {
     const resolve = (data) => {
       this.#changeState(FULFILLED, data)
@@ -25,23 +26,120 @@ class MyPromise {
     }
     
   }
+  
   #changeState(state, data){
     if(this.#state !== PENDING) {
       return
     }
-    this.#state = state;
-    this.#result = data;
-    console.log(this.#state, this.#result)
+    // resolve、reject入参为 Promise
+    if(this.#isPromise(data)) {
+      data.then((res) => {
+        this.#state = FULFILLED;
+        this.#result = res
+      }, (error) => {
+        this.#state = REJECTED;
+        this.#result = error
+      })
+    } else {
+      this.#state = state;
+      this.#result = data;
+    }
+    
+    // console.log(this.#state, this.#result)
+    // 防止 异步执行 resolve 或者reject ,  setTimeOut(() => { resolve() }, 0)
+    this.#run();
   }
-
+  #runOne(callback, resolve, reject) {
+    this.#runMicroTask(() => {
+      if(typeof callback !== 'function') {
+        const fn = this.#state === FULFILLED ? resolve : reject;
+        fn(this.#result)
+        return
+      }
+      try {
+        const data = callback(this.#result);
+        if(this.#isPromise(data)) {
+          data.then(resolve, reject)
+        } else {
+          resolve(data);
+        }
+        
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  #run() {
+      if(this.#state === PENDING) return;
+      while(this.#handlers.length > 0) {
+        const {
+          OnFulfilled,
+          OnRejected,
+          resolve,
+          reject
+        } = this.#handlers.shift();
+          if(this.#state === FULFILLED) {
+            this.#runOne(OnFulfilled, resolve, reject)
+          } else {
+            this.#runOne(OnRejected, resolve, reject)
+          }
+      }
+  }
+  then(OnFulfilled, OnRejected) {
+    return new MyPromise((resolve, reject) => {
+      this.#handlers.push({
+        OnFulfilled,
+        OnRejected,
+        resolve,
+        reject
+      })
+      this.#run()
+    })
+  }
+  // 判断值是否满足Promise A+规范
+  #isPromise(val) {
+    // Promise A+ 规范：一个对象或者函数有一个属性为then的函数
+    if(val !== null && (typeof val === 'object' || typeof val === 'function')) {
+      return typeof val.then === 'function'
+    }
+    return false;
+  }
+  // 将任务放到微队列执行 then方法里面的函数需要放到微队列执行
+  #runMicroTask(func) {
+    // node
+    if(typeof process === 'object' && typeof process.nextTick === 'function') {
+      process.nextTick(func)
+    } else if(typeof MutationObserver === 'function') {
+      // 浏览器环境
+      const ob = new MutationObserver(func);
+      const textNode = document.createTextNode('1');
+      ob.observe(textNode, {
+        characterData:  true,
+      })
+      textNode.data = '2';
+    } else {
+      setTimeout(fun, 0)
+    }
+  }
 }
 
-const promise = new MyPromise((resolve, reject) => {
-  // resolve(123);
-  // reject('111')
-  setTimeout(() => {
-    throw 123
-  }, 0)
-})
-console.log(promise)
+// const promise = new MyPromise((resolve, reject) => {
+//   resolve(123);
+//   reject('111')
+//   // setTimeout(() => {
+//   //   throw 123
+//   // }, 0)
+// })
+// console.log(promise)
 
+// setTimeout(() => {
+//   console.log('111')
+// }, 0);
+
+// new MyPromise((resolve, reject) => {
+//   resolve('222')
+// }).then(res => {
+//   console.log(res)
+// })
+
+// console.log('333')
--- promise/test.js
+++ promise/test.js
@@ -1,7 +1,21 @@
+// const p1 = new Promise(function(resolve, reject){
+//   setTimeout(resolve, 100, 'done')
+// })
+// p1.then(resolve => {
+//   console.log('resolve:', resolve)
+//   console.log('p1:', p1)
+// })
+// console.log(p1)
+
 const p1 = new Promise(function(resolve, reject){
-  setTimeout(resolve, 100, 'done')
+  resolve('123')
 })
-p1.then(resolve => {
-  console.log('resolve:', resolve)
-  console.log('p1:', p1)
+const a = p1.then((res) => {
+  console.log('res:', res)
+  return 1
+}).then((res2) => {
+  console.log('res2:', res2)
+}, (error) => {
+  console.log('error:', error)
 })
+console.log('a:', a)
