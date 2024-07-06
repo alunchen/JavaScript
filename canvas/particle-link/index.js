48a6ece147fb2de9fcd3dfadfe8fe1297dc10561
cal1995 <631439107@qq.com>
2024-07-06 13:50:45 +0800

    fix: 粒子连接

--- canvas/particle-link/index.js
+++ canvas/particle-link/index.js
@@ -1,20 +1,118 @@
 
-const cvs = document.getElementById('cvs');
-const ctx = cvs.getContext('2d');
 
-console.log(ctx)
 
-function init() {
-  cvs.width = window.innerWidth;
-  cvs.height = window.innerHeight
+
+class particalLink {
+  constructor() {
+    this.ctx = null;
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.points = []
+  }
+  initCvs() {
+    const cvs = document.getElementById('cvs');
+    cvs.width = window.innerWidth;
+    cvs.height = window.innerHeight
+    //
+    this.ctx = cvs.getContext('2d');
+    this.ctx.fillStyle = 'rgb(200, 200, 200)'
+   
+  }
+  initPoints(maxLength = 30) {
+    const resultPoints = [];
+    for(let i = 0; i < maxLength; i++) {
+      const point = this.getRandomPoint();
+      resultPoints.push(point)
+    }
+    this.points = resultPoints;
+  }
+  getRandomPoint() {
+    const { width, height } = this;
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    return {
+      x,
+      y
+    }
+  }
+  // 计算两个坐标点之间的距离
+  getPointDis(p1, p2) {
+    const { x: x1, y: y1 } = p1;
+    const { x: x2, y: y2 } = p2;
+    const dis = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    return dis;
+  }
+  drawPotins(maxDis = 400) {
+    const { points, ctx } = this;
+    for(let i = 0; i < points.length; i++) {
+      const p1 = points[i];
+      ctx.beginPath();
+      ctx.arc(p1.x, p1.y, 5, 0, Math.PI * 2);
+      ctx.fill();
+      for(let j = i+1; j < points.length; j++) {
+        const p2 = points[j];
+        const d = this.getPointDis(p1, p2);
+        if(d > maxDis) {
+          continue;
+        }
+        ctx.beginPath();
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.closePath();
+        ctx.strokeStyle = `rgba(200, 200, 200, ${1 - d/maxDis})`;
+        ctx.stroke();
+      }
+    }
+  }
+  
+  init() {
+    this.initCvs();
+    this.initPoints();
+    this.drawPotins()
+  }
 }
-init()
 
-ctx.strokeStyle = '#fff';
+const partical = new particalLink();
+partical.init();
+
+
+// ctx.beginPath();
+// ctx.moveTo(100, 100);
+// ctx.lineTo(200, 200);
+// ctx.lineTo(300, 500);
+// ctx.closePath();
+// ctx.stroke();
+
+/**
+ctx.beginPath();
+ctx.moveTo(100, 200);
+ctx.lineTo(200, 300);
+ctx.stroke()
 
 ctx.beginPath();
-ctx.moveTo(100, 100);
-ctx.lineTo(200, 200);
-ctx.lineTo(300, 500);
-ctx.closePath();
-ctx.stroke();
+ctx.arc(200, 300, 50, 0, Math.PI * 2, true);
+
+ctx.arc(300, 300, 50, 0, Math.PI * 2, true);
+ctx.stroke()
+
+function getRandomPoint() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  return {
+    x,
+    y
+  }
+}
+
+function getPoints(maxLength = 100) {
+  const resultPoints = [];
+  for(let i = 0; i < maxLength; i++) {
+    const point = getRandomPoint();
+    resultPoints.push(point)
+  }
+  console.log(resultPoints)
+  return resultPoints;
+}
+ */
---
+++ canvas/particle-link/main.js
@@ -0,0 +1,62 @@
+/**
+ * 生成随机范围内的整数
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
+const getRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max + 1 - min) + min)
+}
+
+const canvas = document.getElementById('cvs');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+class Point {
+  constructor() {
+    this.r = 6;
+    this.x = getRandomNum(0, window.innerWidth - this.r/2);
+    this.y = getRandomNum(0, window.innerHeight - this.r/2);
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+    ctx.fillStyle = 'rgb(200, 200, 200)';
+    ctx.fill();
+  }
+}
+
+class Graph {
+  constructor(pointLength = 30, maxDis = 400) {
+    this.points = new Array(pointLength).fill(0).map(() => new Point());
+    this.maxDis = maxDis;
+  }
+  lineTo(p1, p2) {
+    const { x: x1, y: y1 } = p1;
+    const { x: x2, y: y2 } = p2;
+    const d = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    if(d > this.maxDis) {
+      return
+    }
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = `rgba(200, 200, 200, ${1 - d/this.maxDis})`;
+    ctx.stroke()
+  }
+  draw() {
+    const { points } = this;
+    for(let i = 0; i < points.length; i++) {
+      const p1 = points[i];
+      p1.draw();
+      for(let j = i + 1; j < points.length; j++) {
+        const p2 = points[j];
+        this.lineTo(p1, p2);
+      }
+    }
+  }
+}
+
+const g = new Graph();
+g.draw()
